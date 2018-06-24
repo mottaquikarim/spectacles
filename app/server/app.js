d2148c169b1aa91a16a46b2feaeee863a6ca668f
Mottaqui Karim <taqqui.karim@gmail.com>
2018-06-24 18:03:47 -0400

    implemented POST method for updating file content

--- app/server/app.js
+++ app/server/app.js
@@ -1,5 +1,6 @@
 // npm level imports
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // module level exports
 const {
@@ -34,6 +35,16 @@ const hydrateResp = content => Object.keys(TEMPLATES).reduce((obj, name, i) => {
     }, {});
 
 /*
+ *  @function statProblem 
+ *  @desc check if problem exists, otherwise throw error 
+ */
+const statProblem = uuid => {
+    const problemDirName = `${CONTENT}/${uuid}`;
+    return fsStat(problemDirName)
+        .then(_ => readContent(problemDirName));
+};
+
+/*
  *  @function runApp 
  *  @desc bootstrap server 
  */
@@ -41,12 +52,28 @@ const runApp = () => new Promise((resolve) => {
     const app = express();
 
     app.use(express.static('public'));
+    app.use(bodyParser.json());
 
-    app.get('/content/:uuid', (req, res) => {
-        const problemDirName = `${CONTENT}/${req.params.uuid}`;
-        fsStat(problemDirName)
-            .then(_ => readContent(problemDirName))
-            .then(content => res.json(hydrateResp(content)))
+    app.get('/content/:uuid', (req, res) => statProblem(req.params.uuid)
+        .then(content => res.json(hydrateResp(content)))
+        .catch(err => res.status(400).json({"err": err})));
+
+    app.post('/content/:uuid', (req, res) => {
+        statProblem(req.params.uuid)
+            .then(content => hydrateResp(content))
+            .then(hydratedContent => {
+                const reqBody = Object.keys(req.body).reduce((hash, key) => {
+                    if (hash[key]) {
+                        hash[key] = req.body[key];
+                    }
+                    return hash;
+                }, hydratedContent);
+
+                return Promise.all(Object.keys(reqBody).map(key => {
+                    return fsWrite(`${CONTENT}/${req.params.uuid}/${key}`, reqBody[key]); 
+                }));
+            })
+            .then(_ => res.json({"success": true}))
             .catch(err => res.status(400).json({"err": err}));
     });
 
