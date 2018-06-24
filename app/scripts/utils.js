79e3f262bdd4dd3fbd50391d34dc8d880fcb2675
Mottaqui Karim <taqqui.karim@gmail.com>
2018-06-24 15:38:49 -0400

    moved fs functions to utils, added support for generating / updating .contentrc

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,3 +1,11 @@
+// npm level imports
+const fs = require('fs');
+
+/*
+ *  @function promisify 
+ *  @desc takes standard nodejs asynch method
+ *        and converts to promise
+ */
 function promisify(namespace, method) {
     const func = namespace[method];
     return (...args) => new Promise((resolve, reject) => {
@@ -13,6 +21,48 @@ function promisify(namespace, method) {
     });
 }
 
+/*
+ *  @function fsStat
+ *  @desc creates a promisified version of fs.stat
+ */
+const fsStat = promisify(fs, 'stat');
+/*
+ *  @function fsMkdir
+ *  @desc creates a promisified version of fs.mkdir
+ */
+const fsMkdir = promisify(fs, 'mkdir');
+
+/*
+ *  @function mkdirp 
+ *  @desc assert that dir exists, if not, create 
+ */
+const mkdirp = dir => fsStat(dir).catch(err => fsMkdir(dir));
+
+/*
+ *  @function fsWrite
+ *  @desc return promise for writing file 
+ */
+const fsWrite = promisify(fs, 'writeFile');
+
+/*
+ *  @function fsRead
+ *  @desc return promise for read file 
+ */
+const fsRead = promisify(fs, 'readFile');
+
+/*
+ *  @function fsWriteIfExists
+ *  @desc return promise for writing file if exists
+ */
+const fsWriteIfExists = (fileName, content="") => fsRead(fileName)
+    .catch(err => fsWrite(fileName, content));
+
 module.exports = {
     promisify,
+    fsMkdir,
+    fsRead,
+    fsStat,
+    fsWrite,
+    fsWriteIfExists,
+    mkdirp,
 };
