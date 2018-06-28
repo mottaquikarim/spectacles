e64492a1573a4e68190f904ce1a39a2550c2f702
Mohammed <c4q@c4qs-MacBook-Pro-5.local>
2018-06-27 21:16:58 -0400

    implemented helper method to open in browser, applied to all script targets

--- app/scripts/update-problem.js
+++ app/scripts/update-problem.js
@@ -1,5 +1,4 @@
 // module level exports
-const opn = require('opn');
 const {runApp} = require('../server/app');
 
 const {
@@ -19,12 +18,13 @@ const updateProblem = (argv) => {
   const problemDirName = `${CONTENT}/${uuid}`;
 
   // UUID Error Checks
-  if (!uuid) { console.log("Error: Please select a UUID."); return; } 
-  fsStat(problemDirName)
-    .then(runApp())
-    .then(_ => {
-        console.log(`Launching: http://localhost:3000/?uuid=${uuid}`);
-        opn(`http://localhost:3000/?uuid=${uuid}`);
+  if (!uuid) { 
+    return Promise.reject("Error: Please select a UUID."); 
+  } 
+
+  return fsStat(problemDirName)
+    .then(() => {
+      return uuid;
     })
     .catch(err => { 
         console.log(`Error: ${problemDirName} doesn't exist. Please make sure you pulled from repo.`); 
