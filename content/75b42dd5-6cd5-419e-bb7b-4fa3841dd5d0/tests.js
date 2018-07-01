8463604d598bfaee6e2e4ee9b6c1b0b066c187ac
Mottaqui Karim <taqqui.karim@gmail.com>
2018-07-01 17:10:50 -0400

    only push contentrc script in master branch and removed extraneous content folders

--- content/75b42dd5-6cd5-419e-bb7b-4fa3841dd5d0/tests.js
+++
@@ -1,8 +0,0 @@
-
-/* 
- * FILENAME:    tests.js
- * DESC:        write any tests that much be asserted for this problem to
- *              be considered successfully completed
- * TEST RUNNER: mocha/chai.js
- */
-    
