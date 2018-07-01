f857bd67c75d43ab5b762c82875a23a745f6e5f1
Mottaqui Karim <taqqui.karim@gmail.com>
2018-07-01 15:17:01 -0400

    attempt to set up codecoverage via codeclimate

--- docker/spectacles/test.sh
+++ docker/spectacles/test.sh
@@ -2,7 +2,24 @@
 
 set -exo pipefail
 
+# Only run if CC_TEST_REPORTER_ID is set
+[ -z "${CC_TEST_REPORTER_ID}" ] || {
+  # CodeClimate requires GIT_COMMITTED_AT to be set
+  # ( when it can't read this from .git )
+  # I'm not sure how it's actually used
+  # or if making it up is bad in any way
+  GIT_COMMITTED_AT=$(date +%s)
+  export GIT_COMMITTED_AT
+  echo $GIT_BRANCH
+  echo $GIT_COMMIT_SHA
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+  chmod +x ./cc-test-reporter
+  ./cc-test-reporter before-build
+} || true
+
 npm test
+
+[ -z "${CC_TEST_REPORTER_ID}" ] || ./cc-test-reporter after-build --debug --exit-code ${EXIT_CODE} || true
 
 EXIT_CODE=$?
 exit ${EXIT_CODE}
