bdff98e8379bdd14b6bae5a5ea07a969dee9d7fa
Mottaqui Karim <taqqui.karim@gmail.com>
2018-07-08 04:44:01 -0400

    header now shows current working branch

--- app/src/js/components/Header.js
+++ app/src/js/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Header = () => (<nav className="navbar navbar-expand-lg navbar-dark">
+const mapStateToProps = state => {
+  const {selected_branch} = state.genericReducer;
+
+  return {
+    selected_branch,
+  }
+};
+
+const selectedBranch = selected_branch => (<div>
+    {selected_branch ? (<div><strong>CURRENT BRANCH: </strong>{selected_branch}</div>) : null}
+</div>)
+
+const ConnectedHeader = ({selected_branch}) => (<nav className="navbar navbar-expand-lg navbar-dark">
     <Link className="navbar-brand" to="/">SPECTACLES</Link>
     <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
@@ -9,7 +22,11 @@ const Header = () => (<nav className="navbar navbar-expand-lg navbar-dark">
                 <a className="nav-link" href="#"></a>
             </li>
         </ul>
+        <div className="form-inline my-2 my-lg-0">
+            {selectedBranch(selected_branch)}
+        </div>
     </div>
 </nav>);
 
+const Header = connect(mapStateToProps)(ConnectedHeader);
 export default Header;
