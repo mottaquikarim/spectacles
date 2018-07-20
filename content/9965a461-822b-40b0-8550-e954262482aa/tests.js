7db9f8ef78a39d09734adaf6f6da05d4c2c9b964
Eric Chen <ericmchen1@gmail.com>
2018-07-20 19:14:48 -0400

    Updating problem 9965a461-822b-40b0-8550-e954262482aa

---
+++ content/9965a461-822b-40b0-8550-e954262482aa/tests.js
@@ -0,0 +1,8 @@
+
+/* 
+ * FILENAME:    tests.js
+ * DESC:        write any tests that much be asserted for this problem to
+ *              be considered successfully completed
+ * TEST RUNNER: mocha/chai.js
+ */
+    
