b253d454104059f534e872bfac5d8e44e24b6005
laurenp34 <36080453+laurenp34@users.noreply.github.com>
2018-07-20 10:43:41 -0400

    Updating problem 147a9488-2f6c-40c6-9801-a1e49397ce17

--- content/147a9488-2f6c-40c6-9801-a1e49397ce17/prompt.js
+++ content/147a9488-2f6c-40c6-9801-a1e49397ce17/prompt.js
@@ -1,9 +1,119 @@
-
-/* 
- * FILENAME:    prompt.js
- * DESC:        write any starter code required here 
- function addTwoNumbers(a,b) {
+/*
+    @func addTwoNumbers
+    @param {number} a
+    @param {number} b
+    @returns {number}
+    @desc - adds two numbers and returns the result
     
- }
- */
+    @example addTwoNumbers(1,2) // 3
+    @example addTwoNumbers(1) // 1
+*/
+
+function addTwoNumbers (a,b){
+    return (a + b)
+}
+
+/*
+    @func turnNumberToString
+    @param {number} a
+    @returns {string}
+    @desc - turns a number into string
+    
+    @example turnNumberToString(1); // "1"
+    @example turnNumberToString("2"); // "2"
+*/
+
+/*
+    @func fullName
+    @param {string} firstName
+    @param {string} lastName
+    @returns {string}
+    @desc - concatenates first and last name
+    
+    @example fullName('Taq', 'Karim'); // "Taq Karim"
+    @example fullName('Foo'); // "Foo"
+*/
+
+/*
+    @func fullNameSentence
+    @param {string} firstName
+    @param {string} lastName
+    @param {string} restOfSentence
+    @desc - takes firstName, lastName and concatenates with
+            restOfSentence
+            USE THE `fullName` function to compute the firstName +
+            lastName portion
+            
+    @example 
+        // expect: "John Smith is awesome"
+        fullNameSentence('John', 'Smith', 'is awesome');
+*/
+
+/*
+    @func fullNameSentenceWithChecks
+    @param {string} firstName
+    @param {string} lastName
+    @param {string} restOfSentence
+    @desc - takes firstName, lastName and concatenates with
+            restOfSentence
+            USE THE `fullName` function to compute the firstName +
+            lastName portion
+            IF `lastName` or `restOfSentence` are not defined
+            return 'Required variables are not set!'
+            
+    @example 
+        // expect: "John Smith is awesome"
+        fullNameSentence('John', 'Smith', 'is awesome');
+    @example
+        // expect: 'Required variables are not set!'
+        fullNameSentenceWithChecks('John', 'Smith');
+*/
+
+/*
+    @func fToC
+    @param {number} f
+    @returns {number}
+    @desc - converts farenheit to celsius
+    
+    @example fToC( 32 ); // 0 
+    @example fToC( 212 ); // 100
+    @example fToC(); // 0
+    
+*/
+
+/*
+    @func fToKelvin
+    @param {number} f
+    @returns {number}
+    @desc - converts farenheit to kelvin
+            conversion factor is this:
+            k = c + 273.15
+            USE THE `fToC` function you wrote to calculate
+            the `c`, then plug that in to the equation above
+            to calculate k
+            
+    @example fToC( 32 ); // 273.15
+    @exmple fToC( 212 ); // 373.15
+    @example fToC(); // 273.15
+*/
+
+/*
+    @func fToKelvinWithChecks
+    @param {number} f
+    @returns {number}
+    @desc - converts farenheit to kelvin
+            conversion factor is this:
+            k = c + 273.15
+            USE THE `fToC` function you wrote to calculate
+            the `c`, then plug that in to the equation above
+            to calculate k
+            IF `f` is not passed in, return "ERROR: variable 'f' is not set"
+            
+    @example fToC( 32 ); // 273.15
+    @exmple fToC( 212 ); // 373.15
+    @example fToC(); // "ERROR: variable 'f' is not set"
+*/
+
+
+
     
--- content/147a9488-2f6c-40c6-9801-a1e49397ce17/tests.js
+++ content/147a9488-2f6c-40c6-9801-a1e49397ce17/tests.js
@@ -6,3 +6,26 @@
  * TEST RUNNER: mocha/chai.js
  */
     
+  
+var assert = require('assert');
+
+
+describe('BasicsI', function(){
+  describe("#addTwoNumbers(a, b)", function() {
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(2, 9), 11);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(10, 3), 13);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(10, 10), 20);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(0, -3), -3);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(3), 3);
+    });
+  })
+})
