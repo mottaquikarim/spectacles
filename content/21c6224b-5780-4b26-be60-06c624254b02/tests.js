3e6b9acf126795ffbb332c7ec18c8f2d76f55928
laurenp34 <36080453+laurenp34@users.noreply.github.com>
2018-07-26 09:51:11 -0400

    Updating problem 21c6224b-5780-4b26-be60-06c624254b02

--- content/21c6224b-5780-4b26-be60-06c624254b02/tests.js
+++ content/21c6224b-5780-4b26-be60-06c624254b02/tests.js
@@ -6,3 +6,55 @@
  * TEST RUNNER: mocha/chai.js
  */
     
+//var assert = require('assert');
+
+describe('1. newShoppingListItem', () => {
+    it('should return an object with item and price attributes', () => {
+        const shoppingListItem = newShoppingListItem('test', 1)
+        chai.assert.equal(shoppingListItem.item, 'test');
+        chai.assert.equal(shoppingListItem.price, 1);
+    });
+});
+
+console.log('hello');
+
+describe('BASICS I', function(){
+  describe("#addTwoNumbers(a, b)", function() {
+    it('should return the sum' , function() {
+      chai.assert.equal(addTwoNumbers(2, 9), 11);
+    });
+    it('should return the sum' , function() {
+      chai.assert.equal(addTwoNumbers(10, 3), 13);
+    });
+    it('should return the sum' , function() {
+      chai.assert.equal(addTwoNumbers(0, 0), 0);
+    });
+    it('should return the sum' , function() {
+      chai.assert.equal(addTwoNumbers(2.5, (-3), (-5.5)));
+    });
+    it('should return the sum' , function() {
+      chai.assert.equal(addTwoNumbers((-92), (-9), (-101)));
+    });
+  })
+  
+  describe("#addTwoNumbers(a, b)", function() {
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(2, 9), 11);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(10, 3), 13);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(0, 0), 0);
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers(2.5, (-3), (-5.5)));
+    });
+    it('should return the sum' , function() {
+      assert.equal(addTwoNumbers((-92), (-9), (-101)));
+    });
+  })
+})
+})
+
+d
