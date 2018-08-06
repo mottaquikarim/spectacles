29fb0d8de540feeb87984f77da806d1db0da1c5c
laurenp34 <36080453+laurenp34@users.noreply.github.com>
2018-08-06 16:15:28 -0400

    Updating problem fdaaff9b-0472-4529-afd5-c48cf1eab777

--- content/fdaaff9b-0472-4529-afd5-c48cf1eab777/prompt.js
+++ content/fdaaff9b-0472-4529-afd5-c48cf1eab777/prompt.js
@@ -4,3 +4,120 @@
  * DESC:        write any starter code required here 
  */
     
+    
+//it's missing the first 2 questions: isUndefined(a) and isNameTooLong(name)
+
+/*
+    @func shouldTakeUmbrella
+    @param {number} chanceOfRain
+    @returns {boolean}
+    @desc - if `chanceOfRain` > 0.5, return true
+            if `chanceOfRain`  > 1, return
+                "ERROR: chanceOfRain must be less than 1"
+
+    @example shouldTakeUmbrella(0.4); // false
+    @example shouldTakeUmbrella(0.8); // true
+    @example shouldTakeUmbrella( 9 ); // "ERROR: chanceOfRain must be less than 1"
+    @example shouldTakeUmbrella(); // "ERROR: chanceOfRain must be less than 1"
+    @example shouldTakeUmbrella( -8 ); // "ERROR: chanceOfRain must be greater than 0"
+    @example shouldTakeUmbrella( -0.3 ); // "ERROR: chanceOfRain must be greater than 0"
+*/
+
+
+/*
+    @func childrensSubtraction
+    @param {number} a
+    @param {number} b
+    returns {number || boolean}
+    @desc - write a function that helps a 3rd grader do his math hw
+            they haven't learned about integers yet,
+
+            so subtracting a - b when a < b is impossible in their world
+
+            IF a < b, return false
+            otherwise, compute the difference and return that instead
+
+    @example childrensSubtraction( 1, 2 ); // false
+    @example childrensSubtraction( 2, 1 ); // 1
+    @example childrensSubtraction( 2 ); // "ERROR: b is not defined"
+*/
+
+/*
+    @func fizzbuzz
+    @param {number} f
+    @returns {string}
+    @desc -
+        Write a function that takes an integer as an argument and
+        determines whether it is a multiple of 3, 5 or both.
+
+        If the number is divisible by 3,
+            return the string 'fizz';
+        If the number is divisible by 5,
+            return the string 'buzz';
+        If the number is divisible by 3 and 5,
+            return the string 'fizzbuzz';
+        If the number is not divisible by 3 nor 5,
+            return the string 'Not fizz nor buzz!'
+
+    @example fizzbuzz( 1 ); // 'Not fizz nor buzz!'
+    @example fizzbuzz( 15 ); // 'fizzbuzz'
+*/
+
+
+
+/*
+    @func isNumber
+    @param potentialNum
+    @returns {boolean}
+    @desc - using the google, determine how you could determine if a
+            value is a __valid__ number
+
+            if it IS a number, return true
+
+    @example isNumber( 1 ); // true
+    @example isNumber( 'taq' ) // false
+    @example isNumber( "3" ) // true
+*/
+
+/*
+    @func stringEquality
+    @param {string} a
+    @param {string} b
+    @returns {boolean}
+    @desc - takes in two strings, determines if they are equal
+
+    @example stringEquality('foo', 'foo') // true
+    @example stringEquality('bar', 'foo') // false
+    @example stringEquality( 'bar', '   bar' ) // true
+    @example stringEquality('bAr', 'BAR') // true
+*/
+
+
+/*
+    @func describeWeather
+    @param {boolean} isItCloudyOutside
+    @param {boolean} isItHotOutside
+    @returns {string}
+    @desc -
+        IF
+            isItCloudyOutside is false
+        AND
+            IsItHotOutside is true
+        SHOW:
+            It is beautiful today
+
+        IF
+            isItCloudyOutside is false
+        AND
+            IsItHotOutside is false
+        SHOW:
+            It is brisk today
+
+        IF
+            isItCloudyOutside is true
+        AND
+            IsItHotOutside is false
+        SHOW:
+            It is bleak today
+*/
+
--- content/fdaaff9b-0472-4529-afd5-c48cf1eab777/tests.js
+++ content/fdaaff9b-0472-4529-afd5-c48cf1eab777/tests.js
@@ -1,8 +1,146 @@
+describe('Conditionals', function(){
+  describe("#isUndefined(a)", function() {
+    it('should return false when passed an undefined value', function() {
+      assert.equal(isUndefined(undefined), true);
+    });
+    it('should return true when passed any value other than undefined', function() {
+      assert.equal(isUndefined(7), false);
+    });
+    it('should return true when passed any value other than undefined', function() {
+      assert.equal(isUndefined("hello world"), false);
+    });
+    it('should return true when passed any value other than undefined', function() {
+      assert.equal(isUndefined(true), false);
+    });
+    it('should return true when passed any value other than undefined', function() {
+      assert.equal(isUndefined(null), false);
+    });
+  })
+})
 
-/* 
- * FILENAME:    tests.js
- * DESC:        write any tests that much be asserted for this problem to
- *              be considered successfully completed
- * TEST RUNNER: mocha/chai.js
- */
-    
+
+describe('Conditionals', function(){
+  describe("#isNameTooLong(name)", function() {
+    it('should return true when the length of name is greater than 16', function() {
+      assert.equal(isNameTooLong('Mottaqui A. Karim'), true);
+    });
+    it('should return false when the length of name is less than 16', function() {
+      assert.equal(isNameTooLong("John Smith"), false);
+    });
+    it('should return true when the length of name is equal to 16', function() {
+      assert.equal(isNameTooLong("Charles Ho Kuang"), true);
+    });
+  })
+})
+
+
+
+describe('Conditionals', function(){
+  describe("#shouldTakeUmbrella(chanceOfRain)", function() {
+    it('should return false when the chanceOfRain is less than 0.5', function() {
+      assert.equal(shouldTakeUmbrella(0.4), false);
+    });
+    it('should return false when the chanceOfRain is equal to 0,5', function() {
+      assert.equal(shouldTakeUmbrella(0.5), false);
+    });
+    it('should return true when chanceOfRain is greater than 0,5', function() {
+      assert.equal(shouldTakeUmbrella(0.51), true);
+    });
+    it('should return an error when chanceOfRain is greater than 1', function() {
+      assert.equal(shouldTakeUmbrella(1.5), "ERROR: chanceOfRain must be less than 1");
+    });
+    it('should return an error when chanceOfRain is less than 0', function() {
+      assert.equal(shouldTakeUmbrella(-4), "ERROR: chanceOfRain must be less than 1");
+    });
+    it('should return an error when chanceOfRain is undefined', function() {
+      assert.equal(shouldTakeUmbrella(), "ERROR: chanceOfRain must be less than 1");
+    });
+  })
+})
+
+
+describe('Conditionals', function(){
+  describe("#childrensSubtraction(a,b)", function() {
+    it('should return false if a < b', function() {
+      assert.equal(childrensSubtraction(1,5), false);
+    });
+    it('should return an error if b is not defined', function() {
+      assert.equal(childrensSubtraction(2), 'ERROR: b is not defined');
+    });
+    it('should return the difference if a > b', function() {
+      assert.equal(shouldTakeUmbrella(5,1), 4);
+    });
+    it('should return the difference if a = b', function() {
+      assert.equal(shouldTakeUmbrella(5,5), 0);
+    });
+  })
+})
+
+
+describe('Conditionals', function(){
+  describe("#fizzbuzz(f)", function() {
+    it('should return "fizz" if the number is divisible by 3', function() {
+      assert.equal(fizzbuzz(6), 'fizz');
+    });
+    it('should return "buzz" if the number is divisible by 5', function() {
+      assert.equal(fizzbuzz(10), 'buzz');
+    });
+    it('should return "fizzbuzz" if the number is divisible by 3 and 5', function() {
+      assert.equal(fizzbuzz(30), 'fizzbuzz');
+    });
+    it('should return "Not fizz nor buzz!" if the number is not divisible by 3 or 5', function() {
+      assert.equal(fizzbuzz(4), 'Not fizz nor buzz!');
+    });
+  })
+})
+
+describe('Conditionals', function(){
+  describe("#isNumber(potentialNum)", function() {
+    it('should return true if the input is a number', function() {
+      assert.equal(isNumber(6), true);
+    });
+    it('should return true if the input is a number', function() {
+      assert.equal(isNumber('6'), true);
+    });
+    it('should return false if the input is a string', function() {
+      assert.equal(isNumber('lauren'), false);
+    });
+    it('should return false if the input is a boolean', function() {
+      assert.equal(isNumber(true), false);
+    });
+  })
+})
+
+
+describe('Conditionals', function(){
+  describe("#stringEquality(a,b)", function() {
+    it('should return true if the strings are equal', function() {
+      assert.equal(stringEquality('foo','foo'), true);
+    });
+    it('should return true if the strings are equal', function() {
+      assert.equal(stringEquality('foo','  foo'), true);
+    });
+    it('should return true if the strings are equal', function() {
+      assert.equal(stringEquality('fOo','FOO'), true);
+    });
+    it('should return false if the strings are unequal', function() {
+      assert.equal(stringEquality('foo','bar'), true);
+    });
+  })
+})
+
+
+
+describe('Conditionals', function(){
+  describe("#describeWeather(isItCloudyOutside,isItHotOutside)", function() {
+    it('should return "It is beautiful today" if it is not cloudy but is  hot outside', function() {
+      assert.equal(describeWeather(false, true), "It is beautiful today");
+    });
+    it('should return "It is brisk today" if it is neither cloudy nor hot outside', function() {
+      assert.equal(describeWeather(false, false), "It is brisk today");
+    });
+    it('should return "It is bleak today" if it is cloudy but not hot outside', function() {
+      assert.equal(describeWeather(true, false), "It is bleak today");
+    });
+  })
+})
